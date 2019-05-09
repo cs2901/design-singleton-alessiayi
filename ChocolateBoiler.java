e4e5644a25a10dec814069af6bcf4051eda1e9c2
jbellidoutec <36900746+jbellidoutec@users.noreply.github.com>
2019-05-09 09:10:22 -0500

    Create ChocolateBoiler.java

---
+++ ChocolateBoiler.java
@@ -0,0 +1,38 @@
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    public ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if(isEmpty() && isBoiled()){
+            //drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if(!isEmpty() && isBoiled()){
+            //bring the contents to a boil
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
